36c02f7c5e5b750f9e2e4c99ae4adb2f0521e5cd
mehmet <mehmet@108.glb-hizlandirici.ankara.edu.tr>
2017-11-09 13:43:15 +0300

    ul1000 upload

---
+++ vacuum/iocBoot/iocvacuum/st.cmd
@@ -0,0 +1,24 @@
+#!../../bin/linux-x86_64/vacuum
+
+## You may have to change vacuum to something else
+## everywhere it appears in this file
+
+< envPaths
+epicsEnvSet("STREAM_PROTOCOL_PATH","${TOP}/protocols")
+
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/vacuum.dbd"
+vacuum_registerRecordDeviceDriver pdbbase
+
+## Load record instances
+dbLoadTemplate "db/vacuum.substitutions"
+
+drvAsynSerialPortConfigure("SYS-SEC-UL1000","/dev/ttyS0")
+#asynSetTraceMask("SYS-SEC-UL1000",-1,0x9)
+#asynSetTraceIOMask("SYSBL-SEC-UL1000",-1,0x2)
+
+cd "${TOP}/iocBoot/${IOC}"
+iocInit
+startPVAServer
